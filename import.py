bfe3974e93d56bc41ae69690fe3a0b57b35b6e07
chi <karta2957702@gmail.com>
2022-04-20 17:18:24 +0800

    已處理：幾入

--- import.py
+++ import.py
@@ -1,27 +1,23 @@
 from ctypes import sizeof
 from logging.handlers import BaseRotatingHandler
+from pickle import TRUE
 import requests 
 from bs4 import BeautifulSoup 
 import csv
 
 size_str = []
-
+"""
 r = requests.get("https://online.carrefour.com.tw/zh/%E9%A3%B2%E6%96%99%E9%9B%B6%E9%A3%9F/%E8%8C%B6%E9%A3%B2%2F%E5%92%96%E5%95%A1/%E7%B6%A0%E8%8C%B6%2F%E7%83%8F%E9%BE%8D%E8%8C%B6%2F%E5%85%B6%E4%BB%96%E8%8C%B6%E9%A3%B2?start=0#" )
 soup = BeautifulSoup( r.text, "html.parser" ) #將網頁資料以html.parser
 sel = soup.find_all( 'div', class_ = 'commodity-desc' )
 
-#siz = soup.find_all( 'span', class_ = 'packageQty')  # maybe can delete
-#for i in siz :
-    #print( i.select_one("span" ).getText() )
-
-data_siz = soup.select("div.box-img a")
-for data in data_siz :
-    if len( data.get('data-variant').split() ) == 3 :
-        size_str.append( data.get('data-variant').split()[2] )
-        #print( data.get('data-variant').split() )
+span_siz = soup.select("div.box-img")
+for span in span_siz :
+    if span.find( 'span' ) == None :
+        size_str.append( '1入' ) 
     else :
-        size_str.append( "1" )
-    
+        size_str.append( span.find('span').text )
+
 
 price = soup.find_all( 'div', class_ = 'current-price')
 
@@ -36,18 +32,19 @@ for i in range( 0, 217, 24) :
     if r.status_code == 200 :
         print( 'Html get success' )
         soup = BeautifulSoup( r.text, "html.parser" ) #將網頁資料以html.parser
-        sel = soup.find_all( 'div', class_ = 'commodity-desc' )
-        #siz = soup.find_all( 'span', class_ = 'packageQty')
 
-        data_siz = soup.select("div.box-img a")
-        for data in data_siz :
-            if len( data.get('data-variant').split() ) == 3 :
-                size_str.append( data.get('data-variant').split()[2] )
-                #print( data.get('data-variant').split() )
+        sel = soup.find_all( 'div', class_ = 'commodity-desc' )  #取得物品名稱
+
+        size_str.clear()
+        span_siz = soup.select("div.box-img")                    #取得幾入裝
+        for span in span_siz :
+            if span.find( 'span' ) == None :
+                size_str.append( '1入' ) 
             else :
-                size_str.append( "1" )
+                size_str.append( span.find('span').text )
+
         
-        price = soup.find_all( 'div', class_ = 'current-price')
+        price = soup.find_all( 'div', class_ = 'current-price')   #取得價格
         with open ( 'output.csv', 'a', newline = '' ) as csvfile :
             writer = csv.writer( csvfile ) #, delimiter = ' ' ) 
             for s, Qty, money in zip(sel, size_str, price) :
@@ -57,4 +54,4 @@ for i in range( 0, 217, 24) :
    
     else :
         print( 'Html get fail.' ) 
-"""
+
